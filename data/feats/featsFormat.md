07272e22fcfe4b06a89da5dcdd89995b916a0ceb
Chen Xuesong <chenxuesong1128@163.com>
2021-10-27 14:57:54 +0800

    增加sample format的说明

---
+++ data/feats/featsFormat.md
@@ -0,0 +1,7 @@
+Each sample consists of four parts, separated by `\t`. 
+- sample id: int
+- user id: string
+- task id: int
+- feature vector: vector separated by `,`
+- label: string
+    
---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@File        :   __init__  
+@Time        :   2021/10/26 3:26 下午
+@Author      :   Xuesong Chen
+@Description :   
+"""
+
